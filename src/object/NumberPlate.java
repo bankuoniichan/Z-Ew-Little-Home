4b32411bae79c66ad01ced4fbf44567057e76bae
asus <asus@10.201.248.148>
2016-12-09 11:35:24 +0700

    add draw()

--- src/object/NumberPlate.java
+++ src/object/NumberPlate.java
@@ -3,6 +3,14 @@ package object;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.javafx.tk.FontLoader;
+import com.sun.javafx.tk.Toolkit;
+
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import ui.Main;
 
 public class NumberPlate extends Plate {
@@ -22,7 +30,7 @@ public class NumberPlate extends Plate {
 		return this.label == otherPlate.label;
 	}
 
-	public void place(Board board,int x, int y) {
+	public void place(Board board, int x, int y) {
 		this.x = x;
 		this.y = y;
 		board.place(this, x, y);
@@ -62,4 +70,18 @@ public class NumberPlate extends Plate {
 
 	}
 
+	public void draw(GraphicsContext gc, int x, int y) {
+		gc.setFill(Color.WHITESMOKE);
+		gc.setStroke(Color.LAVENDER);
+		gc.fillRoundRect(x, y, 50, 50, 10, 10);
+		
+		Font font = Font.font("Tahoma", FontWeight.SEMI_BOLD, FontPosture.REGULAR, 20);
+		FontLoader fontLoader = Toolkit.getToolkit().getFontLoader();
+		double fontWidth = fontLoader.computeStringWidth("" + label, font);
+		double fontHeight = fontLoader.getFontMetrics(font).getLineHeight();
+		gc.setFont(font);
+		gc.setFill(Color.DODGERBLUE);
+		gc.fillText(""+label, x+(50-fontWidth)/2, y+(50-fontHeight)/2);
+		
+	}
 }
